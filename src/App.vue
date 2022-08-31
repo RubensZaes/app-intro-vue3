cf49a85b4c232d45eaa8a2669189d431875e9f6a
Rubens Zaes <rubenszaes@gmail.com>
2022-08-30 23:33:33 -0300

    Declaração de condicionais

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <TheHeader />
+  <TheHeader v-if="showHeader" />
+
+  <div v-show="showName">
+    Nome: {{ firstName }} <br />
+    Sobrenome: {{ lastName }}
+  </div>
+
+  <div v-if="acessLevel === 'admin'">Usuário: Admin</div>
+  <div v-else-if="acessLevel === 'marketing'">Usuário: Marketing</div>
+  <div v-else>Usuário: Usuário</div>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
@@ -13,6 +22,15 @@ export default {
   components: {
     HelloWorld,
     TheHeader,
+  },
+  data() {
+    return {
+      showHeader: true,
+      firstName: "Rubens Zaes",
+      lastName: "Stanley",
+      showName: true,
+      acessLevel: 'user'
+    };
   },
 };
 </script>
